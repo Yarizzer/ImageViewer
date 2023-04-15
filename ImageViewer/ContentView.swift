58e8edb7e7045f6440692121eb2f2885988b644c
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-15 06:19:32 +0300

    Images added. Split view

--- ImageViewer/ContentView.swift
+++ ImageViewer/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HSplitView {
+            Text("Left")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Text("Right")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .padding()
     }
 }
 
