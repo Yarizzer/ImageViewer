31c8d232aa972659ccff3dce3e72b54432ae2010
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-15 08:24:18 +0300

    ref magic numbers removing

--- ImageViewer/Modules/MainScene/View/ContentView.swift
+++ ImageViewer/Modules/MainScene/View/ContentView.swift
@@ -13,28 +13,35 @@ struct ContentView: View {
             List(0..<data.count, id: \.self, selection: $selectedIndex) { index in
                 Text(data[index])
             }
-            .frame(width: 150)
+            .frame(width: Constants.listWidthValue)
         } detail: {
             if let index = selectedIndex {
                 Image(data[index])
                     .resizable()
                     .scaledToFit()
             } else {
-                Text("Please select an image")
+                Text(Constants.selectionPlaceholder)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
-        .frame(minWidth: 480, minHeight: 320)
+        .frame(minWidth: Constants.navigatorMinDemetions.width, minHeight: Constants.navigatorMinDemetions.height)
     }
     
     private let data = ["nssl0033", "nssl0034", "nssl0041", "nssl0042", "nssl0043", "nssl0045", "nssl0046", "nssl0049", "nssl0051", "nssl0091"]
     
     @State private var selectedIndex: Int?
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    private struct Constants {
+        static let selectionPlaceholder = "Please select an image"
+        static let listWidthValue: CGFloat = 150
+        static let navigatorMinDemetions: (width: CGFloat, height: CGFloat) = (width: 480, height: 320)
     }
 }
