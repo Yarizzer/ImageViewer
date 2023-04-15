caf2a4caf0c7f2fbb07e71a274f8a8e1c1b98911
Yaroslav Abaturov <yarizzer@gmail.com>
2023-04-15 05:54:36 +0300

    Initial

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Yaroslav Abaturov on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
